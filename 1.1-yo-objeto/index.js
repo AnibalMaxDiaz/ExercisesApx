f18225d5db35559f6146f2129c86bc04c0229c65
AnibalMaxDiaz <148412143+AnibalMaxDiaz@users.noreply.github.com>
2025-02-10 22:13:43 -0500

    create yo object

---
+++ 1.1-yo-objeto/index.js
@@ -0,0 +1,28 @@
+const yo = {
+  nombre: "Anibal",
+  apellido: "Max",
+  edad: 46,
+  pais: "Colombia",
+  profesion: "Ingeniero de sistemas",
+  sabeManejar: true,
+  mascotas: ["Chispas", "Opalo"],
+  hobbies: ["Codear", "Jugar video juegos", "Ir al Gym", "Ver películas"],
+  tocaInstrumentos: false,
+  nivelDeIngles: "B2",
+  ciudad: "Bogotá",
+  redesSociales: {
+    linkedIn: true,
+    facebook: true,
+    instagram: true,
+    tiktok: false,
+  },
+  peliculasFavoritas: ["Matrix", "La vida es bella", "Constantine", "El señor de los anillos", "Shreck"],
+  comidasFavoritas: ["Bandeja paisa", "Ensalada rusa", "Pizza"],
+  sabeCocinar: true,
+  diaDeLaSemanaFavorito: "Sabado",
+  objetivosPorCumplir: {
+    cortoPlazo: ["Aprender JavaScript", "bajar de peso", "Crear varias aplicaciones"],
+    largoPlazo: ["Convertirme en FullStack", "Crear mi propia empresa", "Tener mi hogar"]
+  }
+};
+console.log(yo);
