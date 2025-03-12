589e99c465f4b659fd4689af4b3175104db0df80
AnibalMaxDiaz <148412143+AnibalMaxDiaz@users.noreply.github.com>
2025-03-12 18:37:24 -0500

    finish

---
+++ 8.1-forCartas/index.js
@@ -0,0 +1,35 @@
+const palos = ["♥️", "♦️", "♣️", "♠️"];
+const valores = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K",
+];
+
+const mazo = [];
+let indiceGeneral = 0;
+
+for (let i = 0; i < palos.length; i++) {
+  const palo = palos[i];
+  for (let J = 0; J < valores.length; J++) {
+    const valor = valores[J];
+    mazo[indiceGeneral] = `${valor} de ${palo}`;
+    indiceGeneral++;
+  }
+
+  // Acá tenés que usar otro for para recorrer
+  // los valores y generar cada carta
+
+  // [tu lógica]
+}
+
+console.log(mazo);
