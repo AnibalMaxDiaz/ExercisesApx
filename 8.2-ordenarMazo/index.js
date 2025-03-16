f956aab328597646b987477543a55d1cbad23734
AnibalMaxDiaz <148412143+AnibalMaxDiaz@users.noreply.github.com>
2025-03-16 14:44:23 -0500

    finish

--- 8.2-ordenarMazo/index.js
+++ 8.2-ordenarMazo/index.js
@@ -1,3 +1,5 @@
+// Sort cards by deck
+
 const mazoCompleto = [
   'A de ♥️',  '2 de ♥️',  '3 de ♥️',  '4 de ♥️',
   '5 de ♥️',  '6 de ♥️',  '7 de ♥️',  '8 de ♥️',
@@ -17,23 +19,52 @@ const mazoCompleto = [
 // esto nos indica en que orden están las cartas en el mazo completo
 const palos = ["corazones", "diamantes", "treboles", "picas"];
 
-let mazo = new Array(52);
-let index = 0;
-
-
 const mazoPorPalo = { };
 
 for (let i = 0; i < palos.length; i++) {
   const nombreDelPalo = palos[i];
   mazoPorPalo[nombreDelPalo] = [];
-  for (let J = 0; J < 13; J++) {
-    const carta = mazoCompleto[J];
-    console.log(carta);
-  }
-  // Acá tenés que usar otro for que vaya del 0 al 13
-  // para recorrer la porción del mazoCompleto correspondiente
-  // combinando i con el otro iterador vas a tener
-  // la posición del array para obtener la carta
-}
 
+  for (let j = 0; j < 13; j++) {
+    const carta = mazoCompleto[i * 13 + j]; 
+    mazoPorPalo[nombreDelPalo].push(carta); // Agrega la carta al array correspondiente
+  }
+}
 console.log(mazoPorPalo);
+
+// first attemp
+
+// const mazoCompleto = [
+//   'A de ♥️',  '2 de ♥️',  '3 de ♥️',  '4 de ♥️',
+//   '5 de ♥️',  '6 de ♥️',  '7 de ♥️',  '8 de ♥️',
+//   '9 de ♥️',  '10 de ♥️', 'J de ♥️',  'Q de ♥️',
+//   'K de ♥️',  'A de ♦️',  '2 de ♦️',  '3 de ♦️',
+//   '4 de ♦️',  '5 de ♦️',  '6 de ♦️',  '7 de ♦️',
+//   '8 de ♦️',  '9 de ♦️',  '10 de ♦️', 'J de ♦️',
+//   'Q de ♦️',  'K de ♦️',  'A de ♣️',  '2 de ♣️',
+//   '3 de ♣️',  '4 de ♣️',  '5 de ♣️',  '6 de ♣️',
+//   '7 de ♣️',  '8 de ♣️',  '9 de ♣️',  '10 de ♣️',
+//   'J de ♣️',  'Q de ♣️',  'K de ♣️',  'A de ♠️',
+//   '2 de ♠️',  '3 de ♠️',  '4 de ♠️',  '5 de ♠️',
+//   '6 de ♠️',  '7 de ♠️',  '8 de ♠️',  '9 de ♠️',
+//   '10 de ♠️', 'J de ♠️',  'Q de ♠️',  'K de ♠️'
+// ]
+
+// // esto nos indica en que orden están las cartas en el mazo completo
+// const palos = ["corazones", "diamantes", "treboles", "picas"];
+
+// let mazo = new Array(52);
+// let index = 0;
+// const mazoPorPalo = { };
+
+// for (let i = 0; i < palos.length; i++) {
+//  const deck = palos[i];
+//  console.log(deck)
+//  for (let j = 0; j < 13; j++) {
+//   const carta = mazoCompleto[i * 13 + j];
+//   mazoPorPalo[index] = mazoCompleto[carta];
+//   console.log(carta);
+//   index++;
+// }
+
+// }
