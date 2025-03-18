a5534e0da4a4ff6287cfb25b94c5fe852cb2cbba
AnibalMaxDiaz <148412143+AnibalMaxDiaz@users.noreply.github.com>
2025-03-18 17:55:57 -0500

    prueba exitosa

--- 9.4-transformar-texto/index.js
+++ 9.4-transformar-texto/index.js
@@ -1,14 +1,35 @@
+// function transformarFrase(frase) {
+//   // 
+//   const arrayDePalabras = [];  
+//   const fraseMayuscula = frase.map((palabra) => {
+//     let inicial = palabra.charAt(0);
+//     let inicialMayuscula = inicial.toUpperCase();
+//     let resto = palabra.slice(1);
+//     let newPalabra = inicialMayuscula + resto;
+//     arrayDePalabras.push(newPalabra);
+//   });
+//   const oracion = arrayDePalabras.join(" ");  // unir las palabras 
+//   return oracion;
+// }
+
+// function main() {
+//   const frase = ['La', 'vida', 'es', 'como', 'una', 'caja', 'de', 'chocolates'];
+//   const oracionMayuscula = transformarFrase(frase);
+//   console.log(oracionMayuscula); // 'La Vida Es Como Una Caja De Chocolates'
+// }
+// main();
+
+
+// This is the rigth way to do this
 function transformarFrase(frase) {
-  // 
-  const arrayDePalabras = [];  // tu código;
-  const fraseMayuscula = arrayDePalabras.map((palabra) => {
-    let inicial = palabra.charAt(0);
-    // console.log(inicial);
-    let resto = palabra.slice(1);
-    let newPalabra = inicial + resto;
-    arrayDePalabras.push(newPalabra);
+  const fraseMayuscula = frase.map((palabra) => {
+    let inicial = palabra.charAt(0); // Obtener la primera letra
+    let inicialMayuscula = inicial.toUpperCase(); // Convertir a mayúscula
+    let resto = palabra.slice(1).toLowerCase(); // Obtener el resto en minúscula
+    return inicialMayuscula + resto; // Devolver la nueva palabra
   });
-  const oracion = arrayDePalabras.join(" ");  // unir las palabras 
+  
+  const oracion = fraseMayuscula.join(" "); // Unir las palabras
   return oracion;
 }
 
