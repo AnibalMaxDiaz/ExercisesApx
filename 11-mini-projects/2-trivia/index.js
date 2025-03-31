ba8bcfdc05e901ea6ec8fc7e4bcd105485dd116d
AnibalMaxDiaz <148412143+AnibalMaxDiaz@users.noreply.github.com>
2025-03-31 18:34:12 -0500

    test modules

--- 11-mini-projects/2-trivia/index.js
+++ 11-mini-projects/2-trivia/index.js
@@ -1,52 +1,13 @@
 const readline = require("readline");
+import questions from "./questions.js";
+import playTrivia from "./game.js";
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-const questions = [
-  {
-    question: "¿Cuál es la capital de Francia?",
-    options: ["1. Berlín", "2. Madrid", "3. París", "4. Roma"],
-    answer: 3,
-  },
-  {
-    question: "¿Cuántos planetas hay en el sistema solar?",
-    options: ["1. 7", "2. 8", "3. 9", "4. 10"],
-    answer: 2,
-  },
-  {
-    question: "¿En qué año llegó el hombre a la Luna?",
-    options: ["1. 1965", "2. 1969", "3. 1972", "4. 1959"],
-    answer: 2,
-  },
-];
 
-let score = 0;
-let index = 0;
-
-function playTrivia() {
-  if (index < questions.length) {
-    const q = questions[index];
-    console.log(`\nPregunta ${index + 1}: ${q.question}`);
-    q.options.forEach((option) => console.log(option));
-
-    rl.question("Tu respuesta (ingresa el número): ", (respuesta) => {
-      if (parseInt(respuesta) === q.answer) {
-        console.log("\x1b[32m¡Correcto!\x1b[0m");
-        score++;
-      } else {
-        console.log("\x1b[31mIncorrecto.\x1b[0m");
-      }
-      index++;
-      playTrivia(); // Llamar recursivamente para la siguiente pregunta
-    });
-  } else {
-    console.log(`\nTu puntuación final es: ${score} de ${questions.length}`);
-    rl.close();
-  }
-}
 
 console.log("¡Bienvenido a la Trivia de Node.js!");
-playTrivia();
+playTrivia(questions);
