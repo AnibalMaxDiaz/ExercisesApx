9349510dc060e910a0735aca63b1ba597da154ee
AnibalMaxDiaz <148412143+AnibalMaxDiaz@users.noreply.github.com>
2025-10-18 10:32:47 -0500

    Verification code for commit 123abc456def789ghi012jkl345mno678pqrstu9

--- 12.7-http/index.js
+++ 12.7-http/index.js
@@ -4,11 +4,13 @@
 
 //importar el módulo http
 const http = require("http");
+
 //crear el servidor
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
   res.end("Hola, mundo!\n");
 });
+
 //escuchar en el puerto 3000
 server.listen(3000, () => {
   console.log("Servidor corriendo en http://localhost:3000");
